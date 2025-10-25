d7449a04be729b5af6c8437244462d25c2166056
ElwinghL <clement_1996@hotmail.com>
2025-10-25 02:44:36 +0200

    test(Assertion): switching from raise to assert

--- packages/pydebroglie/tests/core/test_quadstate.py
+++ packages/pydebroglie/tests/core/test_quadstate.py
@@ -2,42 +2,26 @@ from pydebroglie.core.quadstate import Quadstate
 
 
 def test_is_yes():
-    if not Quadstate.YES.is_yes():
-        raise AssertionError("DEV ERROR: Should not happen Quadstate.YES is not YES")
+    assert Quadstate.YES.is_yes()
 
 
 def test_is_maybe():
-    if not Quadstate.MAYBE.is_maybe():
-        raise AssertionError("DEV ERROR: Should not happen Quadstate.MAYBE is not MAYBE")
+    assert Quadstate.MAYBE.is_maybe()
 
 
 def test_is_no():
-    if not Quadstate.NO.is_no():
-        raise AssertionError("DEV ERROR: Should not happen Quadstate.NO is not NO")
+    assert Quadstate.NO.is_no()
 
 
 def test_is_contradiction():
-    if not Quadstate.CONTRADICTION.is_contradiction():
-        raise AssertionError(
-            "DEV ERROR: Should not happen Quadstate.CONTRADICTION is not CONTRADICTION"
-        )
+    assert Quadstate.CONTRADICTION.is_contradiction()
 
 
 def test_possible():
-    if not Quadstate.YES.possible():
-        raise AssertionError("DEV ERROR: Should not happen Quadstate.YES is not possible")
-    if not Quadstate.MAYBE.possible():
-        raise AssertionError(
-            "DEV ERROR: Should not happen Quadstate.MAYBE is not possible"
-        )
+    assert Quadstate.YES.possible()
+    assert Quadstate.MAYBE.possible()
 
 
 def test_possibly_not():
-    if not Quadstate.NO.possibly_not():
-        raise AssertionError(
-            "DEV ERROR: Should not happen Quadstate.NO is not possibly_not"
-        )
-    if not Quadstate.MAYBE.possibly_not():
-        raise AssertionError(
-            "DEV ERROR: Should not happen Quadstate.MAYBE is not possibly_not"
-        )
+    assert Quadstate.NO.possibly_not()
+    assert Quadstate.MAYBE.possibly_not()
